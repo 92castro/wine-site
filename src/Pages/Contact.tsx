0f5621c6f32b6c374eb4b00062f309927d12949e
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-27 20:33:25 -0700

    embedded google maps onto the business info card

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -8,7 +8,7 @@ const formCard: CSSProperties = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "space-evenly",
+    justifyContent: "space-between",
     minWidth: "auto",
     minHeight: "80vh",
     marginTop: "15px",
@@ -16,13 +16,16 @@ const formCard: CSSProperties = {
     // backgroundColor: "#606D32",
     backgroundColor: "#a8ad9c",
     borderRadius: "10px",
+    padding: "10px"
 }
 
+// Input Fields Styling
 const label: CSSProperties = {
   backgroundColor: "white",
   width: "80%"
 }
 
+// Business Info Styling
 const info: CSSProperties = {
   display: "flex", 
   flexDirection: "column", 
@@ -48,7 +51,9 @@ export default function Contact() {
         <span>(719) 598-1164</span>
         <span>sales@fermentations.biz</span>
       </p>
-      
+      <p><b>Come See Us</b></p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.428126182952!2d-104.78811748483716!3d38.914190979568055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134ef4c367b6b3%3A0xcd184256fb212d0!2sFermentations%20Home%20Winemaking%20and%20Homebrew%20Center!5e0!3m2!1sen!2sjp!4v1677554341746!5m2!1sen!2sjp" width="300" height="225" loading="lazy">
+      </iframe>
     </div>
     {/* Contact Form */}
     <div style={formCard}>
@@ -69,7 +74,7 @@ export default function Contact() {
       />
       <TextField
         id="filled-hidden-label-small"
-        label="E-Mail Address"
+        label="Email Address"
         variant="filled"
         size="small"
         sx={label}
