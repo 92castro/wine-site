c98e34dfd3a76b180b2246b69950a25553aaf854
92castro <celin92castro@gmail.com>
2023-02-28 15:28:41 -0700

    add hero image on homepage

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,33 @@
+import React, { CSSProperties } from "react";
 
+const hero: CSSProperties = {
+  display:"flex",
+  flexDirection: "column",  
+  backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1491924778227-f225b115dd5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')",
+  height: "40vh",
+  minHeight:"15em",
+  alignSelf: "center",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  alignItems: "center",
+  justifyContent: "center"
+}
+
+const heroText: CSSProperties = {
+  textAlign: "center",
+  color: "white",
+  fontSize:"40px"
+}
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div style={hero}>
+      <div style={heroText}>
+        <p>Enjoy your winemaking hobby responsibly!</p>
+      </div>
+    </div>
+    </>
   )
 }
