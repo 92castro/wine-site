1f649b4d726abe2265a951a7972bac89a8980572
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-02 14:03:37 -0700

    readjusted the style of the contact page, adjusted color and spacing

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -8,49 +8,22 @@ import emailjs from '@emailjs/browser';
 import { useForm } from 'react-hook-form';
 import Image from "../Assets/ContactPhoto.jpeg"
 
-
-//Form Card Styling
-const formCardLarge: CSSProperties = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    minWidth: "auto",
-    minHeight: "100vh",
-    marginTop: "15px",
-    marginBottom: "15px",
-    textAlign: "center",
-    backgroundColor: "#a8ad9c",
-    borderRadius: "10px",
-    padding: "10px"
-}
-
-const container1: CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "space-between"
-}
-
 //Form Card Styling
 const formCard: CSSProperties = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-evenly",
     minWidth: "auto",
-    minHeight: "100%",
-    marginTop: "15px",
-    marginBottom: "15px",
+    minHeight: "100vh",
     textAlign: "center",
-    backgroundColor: "#a8ad9c",
+    backgroundColor: "white",
     borderRadius: "10px",
     padding: "10px"
 }
 
 // Input Fields Styling
 const label: CSSProperties = {
-  backgroundColor: "white",
   width: "80%",
   margin: "10px",
 }
@@ -71,8 +44,6 @@ type FormData = {
 };
 
 export default function Contact() {
-  const width = window.innerWidth;
-  
   const form: any = useRef();
   
   const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>();
@@ -90,168 +61,15 @@ export default function Contact() {
   
   const onSubmit = handleSubmit(data => {sendEmail(data)});
       
-  // if (width < 768) {
-  //   return (
-  //     <>
-  //       {/* Contact Form */}
-  //       <form ref={form} onSubmit={onSubmit}>
-  //       <div style={formCard} key={"green"}>
-  //         <h1 className="contactText">Contact Us For Inquiries, Or Questions About Wine And Beer Making!</h1>
-  //         <TextField
-  //           {...register("firstName", { required: "First name is required *" })}
-  //           id="firstName"
-  //           name="firstName"
-  //           label="First Name"
-  //           variant="filled"
-  //           size="small"
-  //           sx={label}
-  //         />
-  //         <Typography component="small">
-  //           {errors.firstName?.message}
-  //         </Typography>
-  //         <TextField
-  //           {...register("lastName", { required: "Last name is required *" })}
-  //           id="lastName"
-  //           name="lastName"
-  //           label="Last Name"
-  //           variant="filled"
-  //           size="small"
-  //           sx={label}
-  //         />
-  //         <Typography component="small">
-  //           {errors.lastName?.message}
-  //         </Typography>
-  //         <TextField
-  //           {...register(
-  //           "email",
-  //           {required: "Email is required", minLength: { value: 4, message: "Minimum length is 4 characters" }, 
-  //           pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i, message: "invalid email address" }}
-  //           )}
-  //           id="emailAddress"
-  //           name="email"
-  //           label="Email Address"
-  //           variant="filled"
-  //           size="small"
-  //           sx={label}
-  //         />
-  //         <Typography component="small">
-  //           {errors.email?.message}
-  //         </Typography>
-  //         <TextField
-  //           {...register("comment", {
-  //         required: "Comment is required",
-  //         minLength: { value: 50, message: "Minimum length is 50 characters" },
-  //         maxLength: { value: 275, message: "Maximum length is 275 characters" },
-  //       })}
-  //           name="comment"
-  //           label="Comment"
-  //           multiline
-  //           rows={4}
-  //           variant="filled"
-  //           sx={label}
-  //         />
-  //         <Typography component="small">
-  //           {errors.comment?.message}
-  //         </Typography>
-  //         <Button type="submit" sx={{ backgroundColor: "#602827" }} variant="contained" endIcon={<SendIcon />}>
-  //           Send
-  //         </Button>
-  //       </div>
-  //       </form>
-  //       {/* Business Information */}
-  //       <div style={formCard}>
-  //       <p style={info}>
-  //         <span><b>Regular Business Hours</b></span>
-  //         <span>Monday - Thursday: 11am to 7pm</span>
-  //         <span>Fridays: 11am to 6pm</span>
-  //         <span>Saturdays: 9am to 2pm</span>
-  //         <span>Closed On Sundays</span>
-  //       </p>
-  //       <p style={info}>
-  //         <span>5765 North Academy Blvd.</span>
-  //         <span>Colorado Springs, CO 80918</span>
-  //         <span>(719) 598-1164</span>
-  //         <span>sales@fermentations.biz</span>
-  //       </p>
-  //       <p><b>Come See Us</b></p>
-  //       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.428126182952!2d-104.78811748483716!3d38.914190979568055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134ef4c367b6b3%3A0xcd184256fb212d0!2sFermentations%20Home%20Winemaking%20and%20Homebrew%20Center!5e0!3m2!1sen!2sjp!4v1677554341746!5m2!1sen!2sjp" className="map" loading="lazy">
-  //       </iframe>
-  //       </div>
-  //     </>
-  //   )
-  // } else {
-  //     return (
-  //       // Large Screen Component
-  //       <div style={formCardLarge}>
-  //         <form style={container1} ref={form} onSubmit={sendEmail}>
-  //           <h1 style={{ color: "#602827"}} className="contactText">Contact Us For Inquiries, Or Questions About Wine And Beer Making!</h1>
-  //           <TextField
-  //             id="firstName"
-  //             name="first_name"
-  //             label="First Name"
-  //             variant="filled"
-  //             size="small"
-  //             sx={label}
-  //           />
-  //           <TextField
-  //             id="lastName"
-  //             name="last_name"
-  //             label="Last Name"
-  //             variant="filled"
-  //             size="small"
-  //             sx={label}
-  //           />
-  //           <TextField
-  //             id="emailAddress"
-  //             name="email"
-  //             label="Email Address"
-  //             variant="filled"
-  //             size="small"
-  //             sx={label}
-  //           />
-  //           <TextField
-  //             id="commentSection"
-  //             name="comment"
-  //             label="Comment"
-  //             multiline
-  //             rows={4}
-  //             variant="filled"
-  //             sx={label}
-  //           />
-  //           <Button className="button" type="submit" sx={{ backgroundColor: "#602827" }} variant="contained" endIcon={<SendIcon />}>
-  //             Send
-  //           </Button>
-  //         </form>
-  //         <div>
-  //           <p className="info" style={info}>
-  //             <span><b>Regular Business Hours</b></span>
-  //             <span>Monday - Thursday: 11am to 7pm</span>
-  //             <span>Fridays: 11am to 6pm</span>
-  //             <span>Saturdays: 9am to 2pm</span>
-  //             <span>Closed On Sundays</span>
-  //           </p>
-  //           <p className="info" style={info}>
-  //             <span>5765 North Academy Blvd.</span>
-  //             <span>Colorado Springs, CO 80918</span>
-  //             <span><b>Phone:</b> (719) 598-1164</span>
-  //             <span><b>Email:</b> sales@fermentations.biz</span>
-  //           </p>
-  //           <p className="info"><b>Come See Us</b></p>
-  //           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.428126182952!2d-104.78811748483716!3d38.914190979568055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134ef4c367b6b3%3A0xcd184256fb212d0!2sFermentations%20Home%20Winemaking%20and%20Homebrew%20Center!5e0!3m2!1sen!2sjp!4v1677554341746!5m2!1sen!2sjp" className="map" loading="lazy">
-  //           </iframe>
-  //         </div>
-  //       </div>
-  //   )
-  // }
   return (
     <>
     <div className="contactImage">
         <img src={Image} style={{ width: "100%"}} />
         <h3 className="contactText">
-          Contact Us Today!
+          Let's Talk Wine and Beer!
         </h3>
     </div>
-    <Grid container>
+    <Grid container sx={{ display: "flex", alignItems: "center" }}>
       <Grid item sm={12} md={6}>
         <form ref={form} onSubmit={onSubmit} style={{ height: "100%"}}>
         <div style={formCard} key={"green"}>
@@ -260,8 +78,8 @@ export default function Contact() {
             {...register("firstName", { required: "First name is required *" })}
             id="firstName"
             name="firstName"
-            label="First Name"
-            variant="filled"
+            label="First Name*"
+            variant="standard"
             size="small"
             sx={label}
           />
@@ -272,8 +90,8 @@ export default function Contact() {
             {...register("lastName", { required: "Last name is required *" })}
             id="lastName"
             name="lastName"
-            label="Last Name"
-            variant="filled"
+            label="Last Name*"
+            variant="standard"
             size="small"
             sx={label}
           />
@@ -288,8 +106,8 @@ export default function Contact() {
             )}
             id="emailAddress"
             name="email"
-            label="Email Address"
-            variant="filled"
+            label="Email Address*"
+            variant="standard"
             size="small"
             sx={label}
           />
@@ -303,17 +121,17 @@ export default function Contact() {
           maxLength: { value: 275, message: "Maximum length is 275 characters" },
         })}
             name="comment"
-            label="Comment"
+            label="Comment*"
             multiline
             rows={4}
-            variant="filled"
+            variant="standard"
             sx={label}
           />
           <Typography component="small">
             {errors.comment?.message}
           </Typography>
-          <Button type="submit" sx={{ backgroundColor: "#602827" }} variant="contained" endIcon={<SendIcon />}>
-            Send
+          <Button type="submit" sx={{ backgroundColor: "#4a0d0b", width: "30%", marginTop: "50px", marginBottom: "50px" }} variant="contained">
+            Submit
           </Button>
         </div>
         </form>
@@ -321,7 +139,7 @@ export default function Contact() {
       <Grid item xs={12} md={6}>
         <div style={formCard}>
         <p style={info}>
-          <span><b>Regular Business Hours</b></span>
+          <span style={{ color: "#74250e", fontSize: "140%" }}><b>Regular Business Hours</b></span>
           <span>Monday - Thursday: 11am to 7pm</span>
           <span>Fridays: 11am to 6pm</span>
           <span>Saturdays: 9am to 2pm</span>
@@ -330,10 +148,10 @@ export default function Contact() {
         <p style={info}>
           <span>5765 North Academy Blvd.</span>
           <span>Colorado Springs, CO 80918</span>
-          <span>(719) 598-1164</span>
-          <span>sales@fermentations.biz</span>
+          <span><b style={{ color: "#74250e"}}>Phone: </b>(719) 598-1164</span>
+          <span><b style={{ color: "#74250e"}}>Email: </b>sales@fermentations.biz</span>
         </p>
-        <p><b>Come See Us</b></p>
+        {/* <p style={{ color: "#74250e", fontSize: "140%"}}><b>Come See Us</b></p> */}
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.428126182952!2d-104.78811748483716!3d38.914190979568055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134ef4c367b6b3%3A0xcd184256fb212d0!2sFermentations%20Home%20Winemaking%20and%20Homebrew%20Center!5e0!3m2!1sen!2sjp!4v1677554341746!5m2!1sen!2sjp" className="map" loading="lazy">
         </iframe>
         </div>
