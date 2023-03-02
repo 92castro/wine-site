67906b206774f23eb9ba479a1d47df1b3bf70422
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-02 15:14:02 -0700

    added facebook and twitter icons

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -3,6 +3,8 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 import emailjs from '@emailjs/browser';
 import { useForm } from 'react-hook-form';
 import Image from "../Assets/ContactPhoto.jpeg"
@@ -138,6 +140,10 @@ export default function Contact() {
         </Grid>
         <Grid item xs={12} md={6}>
           <div style={formCard}>
+            <div>
+              <FacebookIcon sx={{ fontSize: "300%", margin: "10px" }}/>
+              <TwitterIcon sx={{ fontSize: "300%", margin: "10px" }}/>
+            </div>
             <p style={info}>
               <span style={{ color: "#74250e", fontSize: "140%", fontFamily: "Italiana, serif" }}><b>Regular Business Hours</b></span>
               <span>Monday - Thursday: 11am to 7pm</span>
@@ -151,7 +157,6 @@ export default function Contact() {
               <span><b style={{ color: "#74250e"}}>Phone: </b>(719) 598-1164</span>
               <span><b style={{ color: "#74250e"}}>Email: </b>sales@fermentations.biz</span>
             </p>
-            {/* <p style={{ color: "#74250e", fontSize: "140%"}}><b>Come See Us</b></p> */}
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.428126182952!2d-104.78811748483716!3d38.914190979568055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134ef4c367b6b3%3A0xcd184256fb212d0!2sFermentations%20Home%20Winemaking%20and%20Homebrew%20Center!5e0!3m2!1sen!2sjp!4v1677554341746!5m2!1sen!2sjp" className="map" loading="lazy">
             </iframe>
           </div>
