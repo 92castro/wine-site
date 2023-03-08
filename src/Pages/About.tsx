2fc13825381a3c3138fff8a4ab1d1c901636cc26
92castro <celin92castro@gmail.com>
2023-03-08 14:07:38 -0700

    update homes page and add paragraphs to about page

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,8 +1,9 @@
 
 import React, { CSSProperties } from "react";
 import {Link} from "react-router-dom"
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Grid, Typography, Box } from "@mui/material";
+
+
 
 const heroImg: CSSProperties = {
   display:"flex",
@@ -30,7 +31,7 @@ const title: CSSProperties = {
   color: "var(--maroon-color)",
   fontWeight: "700",
   fontSize: "50px",
-  marginTop:"50px",
+  marginTop:"40px",
   marginBottom:"10px",
   width:"100%",
 }
@@ -41,23 +42,57 @@ const paragraphs: CSSProperties = {
   color:"var(--maroon-color)",
 }
 
+const StyledBox: CSSProperties = {
+  // height: "100px",
+  display: "flex",
+  flexDirection:"column",
+  borderRadius: "8px",
+};
+
 export default function About() {
   return (
-    <>
+  <>
     <div style={heroImg}>
         <div style={heroText}>
           <p></p>
         </div>
     </div>
-        <Typography style={title}>
+    <div style={{padding:"20px"}}>
+    <Grid container minHeight="30vh" maxWidth="lg" margin={{ lg: "auto" }} rowSpacing={{xs:"2",md:"20"}} columnSpacing={{xs:"2",md:"3rem"}}>
+          <Typography style={title}>
           Who we are
         </Typography>
-        <Box>
-          <Typography style={paragraphs}>
-          Since 2011, Fermentation has been serving Colorado Springs, CO with a wide selection of brewing products, equipment, and supplies for both beginners and advanced brewers. We also offer classes and workshops where we will teach you how to make wine & beer you can share with pride. We began our winemaking hobby as a social event for couples, and have continued to enjoy creating exceptional wines with our friends. Our store is dedicated to creating a community of brewing enthusiasts who share our love for the art and providing everything needed to create unique and delicious brews. Visit us to experience the joy of fermentation for yourself!
+          <Grid item xs={12} sm={12} md={12}>
+            <Box style={StyledBox}>
+              <Typography style={paragraphs}>
+                Since 2011, Fermentation has been serving Colorado Springs, CO with a wide selection of brewing products, equipment, and supplies for both beginners and advanced brewers. We also offer classes and workshops where we will teach you how to make wine & beer you can share with pride. We began our winemaking hobby as a social event for couples, and have continued to enjoy creating exceptional wines with our friends. Our store is dedicated to creating a community of brewing enthusiasts who share our love for the art and providing everything needed to create unique and delicious brews. Visit us to experience the joy of fermentation for yourself!
+              </Typography> 
+              </Box>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <Box sx={{height:"100%"}}>
+            <img src="https://www.aws-cos.com/x/cdn/?https://storage.googleapis.com/production-sitelio-v1-0-5/085/213085/jsB24WCV/979969d26bd24a40b5f501ac221ab0cf" alt="Tim & Brenda" style={{width:"100%",height:"100%",marginInline:"auto" }} />
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <Box style={StyledBox}>
+              <Typography style={title}>
+          American Wine Society
         </Typography>
-        <img src="https://www.aws-cos.com/x/cdn/?https://storage.googleapis.com/production-sitelio-v1-0-5/085/213085/jsB24WCV/979969d26bd24a40b5f501ac221ab0cf" alt="Tim & Brenda" style={{width:"450px", height:"500px"}} />
-        </Box>
+              <Typography style={paragraphs}>
+                Fermentations is a proud member and sponsor of the Colorado Springs Chapter of American Wine Society.
+                Visit the AWS website at: <a href="https://www.aws-cos.com/" style={{color:"black"}}>www.aws-cos.com.</a>
+              </Typography>
+              <Typography style={title}>
+          Wine Exploration Groups
+        </Typography>
+              <Typography style={paragraphs}>
+                Contact Brenda to join in the fun, for more details and future announcements. Winemaking is a great way to meet new friends and get together with existing ones. Visit Tim or Brenda for more details on costs, schedules and to Join the Fermentations Wine Explorations! Let's travel the winemaking world together! 
+              </Typography> 
+              </Box>
+          </Grid>
+        </Grid>
+        </div>
     </>
   )
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,7 +8,6 @@ import {Link} from "react-router-dom"
 import Divider from "@mui/material/Divider"
 import { GiCellarBarrels } from 'react-icons/gi';
 import { Typography, Box, Paper, Grid } from "@mui/material";
-import { minHeight, padding, width } from "@mui/system";
 
 const heroImg: CSSProperties = {
   display:"flex",
@@ -36,7 +35,7 @@ const title: CSSProperties = {
   color: "var(--maroon-color)",
   fontWeight: "600",
   fontSize: "40px",
-  marginTop:"50px",
+  // marginTop:"50px",
   marginBottom:"10px",
   width:"100%",
 }
@@ -74,19 +73,18 @@ export default function Home() {
         </div>
       </div>
     <Divider style={{height:"50px"}}><GiCellarBarrels style={{height:"100px",fontSize:"60px"}}/></Divider>
-    <Grid container minHeight="30vh" maxWidth="lg" margin={{ lg: "auto" }} spacing={3} px={2}>
-      <Typography style={title} sx={{fontSize:{xs:"10px"}}}
+    <Grid container minHeight="30vh" maxWidth="lg" margin={{ lg: "auto" }} spacing={5} px={2}>
+      
+      <Grid item xs={12} sm={6} lg={6} >
+        <Box style={StyledBox}>
+          <Typography style={title}
       >
         Fermentations Education Center
       </Typography>
-      <Grid item xs={12} sm={6} lg={6} >
-        <Box style={StyledBox}>
-            {/* <Paper style={StyledPaper} sx={{minHeight:{sm:"200px",md:"245px", lg:"240px"}}}> */}
               <Typography style={paragraphs}>
                 In order to serve our clients better, we have created a new Classroom/Education Center within Fermentations Home Winemaking Center. We have always valued the educational aspects of making wine and beer, so now we have a room dedicated to the learning environment. In the near future, we'll feature wine and beer education events.
               </Typography>
               <Link style={{fontWeight:"550"}} to="/contact">Contact us for arrangements to use this presentation space for your special event. </Link>
-            {/* </Paper> */}
           </Box>
       </Grid>
       <Grid item xs={12} sm={6} lg={5}>
@@ -94,10 +92,7 @@ export default function Home() {
           <img src={class1} alt="classroom" style={images} />
         </Box>
       </Grid>
-      <Typography style={title}
-      >
-        We Have Grains
-      </Typography>
+     
       <Grid item xs={12} sm={6} lg={5}>
         <Box  style={StyledBox}>
           <img src={grains} alt="grainroom" style={images} />
@@ -105,24 +100,25 @@ export default function Home() {
       </Grid>
       <Grid item xs={12} sm={6} lg={6}>
           <Box style={StyledBox}>
-            {/* <Paper style={StyledPaper} sx={{minHeight:{sm:"200px",md:"245px", lg:"235px"}}}> */}
+            <Typography style={title}
+      >
+        We Have Grains
+      </Typography>
               <Typography style={paragraphs}>
-                To serve our all-grain brewing community better, we've expanded and added a full-service grain room, complete with 15 Base Malts, 60 Specialty Malts, and two mills (one Barley Crusher and one Brewery Series Professional mill).  Check out our selection - we intend to have the best selection and service in this Region!
+                To serve our all-grain brewing community better, we've expanded and added a full-service grain room, complete with 15 Base Malts, 60 Specialty Malts, and two mills (one Barley Crusher and one Brewery Series Professional mill). Check out our selection - we intend to have the best selection and service in this Region!
               </Typography>
-            {/* </Paper> */}
           </Box>
       </Grid>
-      <Typography style={title}
+      
+      <Grid item xs={12} sm={6} lg={6} >
+        <Box style={StyledBox}>
+          <Typography style={title}
       >
         Wine Kits
       </Typography>
-      <Grid item xs={12} sm={6} lg={6} >
-        <Box style={StyledBox}>
-            {/* <Paper style={StyledPaper} sx={{minHeight:{sm:"200px",md:"245px", lg:"240px"}}}> */}
               <Typography style={paragraphs}>
                 Are you a wine enthusiast? A wine starter kit is the perfect way to get started on your winemaking journey, providing you with all the essential equipment and ingredients you need to create your first batch of wine. And the best part, you don't need any prior winemaking experience to get started!
               </Typography>
-            {/* </Paper> */}
           </Box>
       </Grid>
       <Grid item xs={12} sm={6} lg={5}>
@@ -130,10 +126,7 @@ export default function Home() {
           <img src={wine} alt="classroom" style={images} />
         </Box>
       </Grid>
-      <Typography style={title}
-      >
-        Beer Kits
-      </Typography>
+      
       <Grid item xs={12} sm={6} lg={5}>
         <Box  style={StyledBox}>
           <img src={beer} alt="bottles" style={images} />
@@ -141,11 +134,13 @@ export default function Home() {
       </Grid>
       <Grid item xs={12} sm={6} lg={6}>
           <Box style={StyledBox}>
-            {/* <Paper style={StyledPaper} sx={{minHeight:{sm:"200px",md:"245px", lg:"235px"}}}> */}
+            <Typography style={title}
+      >
+        Beer Kits
+      </Typography>
               <Typography style={paragraphs}>
                 Are you a beer lover? Imagine the satisfaction of creating your own unique brew, perfectly tailored to your personal taste. With a beer starter kit, you can do just that! Brewing your own beer at home is not only a fun and rewarding hobby, but it also allows you to experiment with different ingredients and styles to create your own signature brew.
               </Typography>
-            {/* </Paper> */}
           </Box>
       </Grid>
     </Grid>
